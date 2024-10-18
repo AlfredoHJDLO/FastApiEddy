3a0b35a2ce2a92a4140497a4af4fc838b74c21a7
Alfredo Miguel Peña Martínez <alfredo.pena@dsindigo.com>
2024-10-18 13:23:34 -0600

    first commit

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, ConfigDict, EmailStr
+
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserDetail(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
